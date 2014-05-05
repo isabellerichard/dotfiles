925e5985174fe2629a28cf43f32c07e5816588a9
Isabelle RICHARD <isabellerichard89@gmail.com>
2014-05-05 21:08:09 +0200

    [FIX] remove useless code from script

--- load.sh
+++ load.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Please configure the location of your dotfiles directory
-SAVE_DIR=$HOME/Workspace/dotfiles
-
 # Prepare the directories
 mkdir -p ~/.ssh ~/.config/terminator
 
 # Do the copy
-cd $SAVE_DIR
 cp bash_profile ~/.bash_profile
 cp bashrc ~/.bashrc
 cp emacs ~/.emacs
