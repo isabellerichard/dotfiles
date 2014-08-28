1aedb6537fdb64300b7896d8d80c9902b69b06e4
Isabelle RICHARD <isabellerichard89@gmail.com>
2014-08-29 00:19:20 +0200

    [FIX] remove unused code

--- load.sh
+++ load.sh
@@ -3,19 +3,6 @@
 # Prepare the directories
 mkdir -p ~/.ssh ~/.config/terminator
 
-# Do the copy
-if false
-then
-    cp bash_profile ~/.bash_profile
-    cp bashrc ~/.bashrc
-    cp emacs ~/.emacs
-    cp gitconfig ~/.gitconfig
-    cp rsync ~/.rsync
-    cp vimrc ~/.vimrc
-    cp ssh.config ~/.ssh/config
-    cp terminator.config ~/.config/terminator/config
-fi
-
 # Replace config files by link to this repo
 ln -sf $PWD/bash_profile $HOME/.bash_profile
 ln -sf $PWD/bashrc $HOME/.bashrc
