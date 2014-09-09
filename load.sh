2edeae65998257bf187196651927217f091854a9
Isabelle RICHARD <isabellerichard89@gmail.com>
2014-09-09 22:19:19 +0200

    [ADD] global gitignore

--- load.sh
+++ load.sh
@@ -8,6 +8,8 @@ ln -sf $PWD/bash_profile $HOME/.bash_profile
 ln -sf $PWD/bashrc $HOME/.bashrc
 ln -sf $PWD/emacs $HOME/.emacs
 ln -sf $PWD/gitconfig $HOME/.gitconfig
+ln -sf $PWD/gitignore_global $HOME/.gitignore_global
+ln -sf $PWD/guake $HOME/.gconf/apps/guake
 ln -sf $PWD/rsync $HOME/.rsync
 ln -sf $PWD/vimrc $HOME/.vimrc
 ln -sf $PWD/ssh.config $HOME/.ssh/config
