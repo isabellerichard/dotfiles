3bf5813fb222ed8f053d938fb4c54ad868313e6c
Isabelle RICHARD <isabellerichard89@gmail.com>
2015-05-15 18:36:48 +0200

    [ADD] Add Subversion configuration file
    Ignore files like .project, .pydevproject, ...

--- load.sh
+++ load.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Prepare the directories
-mkdir -p ~/.ssh ~/.config/terminator
+mkdir -p ~/.ssh ~/.config/terminator ~/.subversion
 
 # Replace config files by link to this repo
 ln -sf $PWD/bash_profile $HOME/.bash_profile
@@ -13,6 +13,7 @@ ln -sf $PWD/gitignore_global $HOME/.gitignore_global
 ln -sf $PWD/pdbrc $HOME/.pdbrc
 ln -sf $PWD/psqlrc $HOME/.psqlrc
 ln -sf $PWD/rsync $HOME/.rsync
+ln -sf $PWD/subversion.config $HOME/.subversion/config
 ln -sf $PWD/vimrc $HOME/.vimrc
 ln -sf $PWD/ssh.config.tmpl $HOME/.ssh/config.tmpl
 ln -sf $PWD/terminator.config $HOME/.config/terminator/config
