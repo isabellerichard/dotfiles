6499b98912dd3a31aea897f954b00c2e57d40aba
Isabelle RICHARD <isabelle.richard@smile.fr>
2016-06-04 12:17:24 +0200

    Rename files with .

--- load.sh
+++ load.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
 # Prepare the directories
-mkdir -p $HOME/.ssh $HOME/.config/terminator $HOME/.odoo $HOME/.subversion
+mkdir -p $HOME/.ssh $HOME/.config/terminator $HOME/.subversion
 
 # Replace config files by link to this repo
-ln -sf $PWD/bash_profile $HOME/.bash_profile
-ln -sf $PWD/bashrc $HOME/.bashrc
-ln -sf $PWD/flake8 $HOME/.config/flake8
-ln -sf $PWD/gitconfig $HOME/.gitconfig
-ln -sf $PWD/gitignore_global $HOME/.gitignore_global
-ln -sf $PWD/multitailrc $HOME/.multitailrc
-ln -sf $PWD/odoorc $HOME/.odoo/.odoorc
-ln -sf $PWD/pdbrc $HOME/.pdbrc
-ln -sf $PWD/pep8 $HOME/.config/pep8
-ln -sf $PWD/profile $HOME/.profile
-ln -sf $PWD/psqlrc $HOME/.psqlrc
-ln -sf $PWD/rsync $HOME/.rsync
-ln -sf $PWD/subversion.config $HOME/.subversion/config
-ln -sf $PWD/vimrc $HOME/.vimrc
-ln -sf $PWD/ssh.config.tmpl $HOME/.ssh/config.tmpl
-ln -sf $PWD/terminator.config $HOME/.config/terminator/config
+cp .bash_profile .bashrc .gitconfig .gitignore_global .multitailrc .pdbrc \
+    .profile .psqlrc .rsync .vimrc $HOME
+cp .flake8 .pep8 $HOME/.config
+cp subversion.config $HOME/.subversion/config
+cp ssh.config.tmpl $HOME/.ssh/config.tmpl
+cp terminator.config $HOME/.config/terminator/config
 
