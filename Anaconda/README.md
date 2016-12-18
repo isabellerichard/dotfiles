cebda21fc5653632843126375d58f1d7a93ba8f6
Isabelle RICHARD <isabelle.richard@smile.fr>
2016-12-18 16:58:17 +0100

    Add Anaconda installation instructions
    Anaconda is the Python IDE for Sublime Text 3

---
+++ Anaconda/README.md
@@ -0,0 +1,17 @@
+# Guide to install Anaconda
+
+## Anaconda is the Python IDE for Sublime Text 3
+
+### Follow instructions to get Anaconda
+
+Download Sublime Text 3 for Ubuntu 64 bit :
+
+* https://www.sublimetext.com/3
+
+Follow instructions to install Package Control : 
+
+* https://packagecontrol.io/installation
+
+Follow instructions to install Anaconda :
+
+* http://damnwidget.github.io/anaconda/#using-anaconda
--- load.sh
+++ load.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Prepare the directories
-mkdir -p $HOME/.ssh $HOME/.config/terminator $HOME/.subversion
+mkdir -p $HOME/.ssh $HOME/.config/sublime-text-3/Packages/User $HOME/.config/terminator $HOME/.subversion
 
 # Replace config files by link to this repo
 cp .bash_profile .bashrc .gitconfig .gitignore_global .multitailrc .pdbrc \
@@ -9,5 +9,5 @@ cp .bash_profile .bashrc .gitconfig .gitignore_global .multitailrc .pdbrc \
 cp flake8 pep8 $HOME/.config
 cp subversion.config $HOME/.subversion/config
 cp ssh.config.tmpl $HOME/.ssh/config.tmpl
+cp Anaconda/Anaconda.sublime-settings $HOME/.config/sublime-text-3/Packages/User
 cp terminator.config $HOME/.config/terminator/config
-
