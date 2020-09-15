bca07e189bf955c11a4531b1c686cdb80c3e76c3
Isabelle RICHARD <isabelle.richard@smile.fr>
2020-09-15 21:13:28 +0200

    [ADD] Add a script to save new configuration files

---
+++ save.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Replace config files by link to this repo
+cp $HOME/.bash_profile $HOME/.bashrc $HOME/.gitconfig $HOME/.gitignore_global \
+    $HOME/.pdbrc  $HOME/.profile $HOME/.psqlrc $HOME/.rsync $HOME/.vimrc .
+cp $HOME/.config/flake8 $HOME/.config/pep8 .
+cp $HOME/.ssh/config.tmpl ssh.config.tmpl
+cp $HOME/.config/terminator/config terminator.config
